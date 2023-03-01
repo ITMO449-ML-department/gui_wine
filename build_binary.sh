b74937e612d208f0839658781b1e3d48dd4e5f71
Kirill Lalayants <lalayantskirill@gmail.com>
2023-03-02 00:21:02 +0300

    binary builder

--- build_binary.sh
+++ build_binary.sh
@@ -3,4 +3,4 @@ pyinstaller -F -n wine --windowed -p . \
 --hidden-import="sklearn.metrics._pairwise_distances_reduction._middle_term_computer" \
 --hidden-import="PIL._tkinter_finder" \
 --hidden-import="scipy.fftpack" \
-shit.py
+for_binary_build.py
